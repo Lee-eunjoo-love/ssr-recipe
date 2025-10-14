8dc01341d2f316aecbc7aeeb6e64a8a885af6dd4
Lee-eunjoo-love <hychunjae2025@gmail.com>
2025-10-15 07:40:29 +0900

    사용자정의 usePreloader Hook 적용

--- src/containers/UserContainer.js
+++ src/containers/UserContainer.js
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import User from "../components/User";
-import { Preloader } from "../lib/PreloadContext";
+import { Preloader, usePreloader } from "../lib/PreloadContext";
 import { getUser } from "../modules/users";
 
 const UserContainer = ({ id }) => {
   const user = useSelector((state) => state.users.user);
   const dispatch = useDispatch();
+
+  // #. 사용자정의 usePreloader Hook 사용하기
+  usePreloader(() => dispatch(getUser(id)));
 
   useEffect(() => {
     if (user && user.id === parseInt(id, 10)) return; // 이미 유효한 사용자이면 요청하지 않음
@@ -15,9 +18,12 @@ const UserContainer = ({ id }) => {
   }, [dispatch, id, user]);
 
   // #. 유효성 검사 후 null 반환해야 하는 경우 null 대신 Preloader 반환
-  if (!user) {
+  /*if (!user) {
     return <Preloader resolve={() => dispatch(getUser(id))} />;
-  }
+  }*/
+  // #. 유효성 검사 후 null 반환해야 하는 경우 null 대신 Preloader 반환하는 로직을 사용자정의 usePreloader Hook 사용시 null 반환
+  if (!user) return null;
+
   return <User user={user} />;
 };
 
--- src/lib/PreloadContext.js
+++ src/lib/PreloadContext.js
@@ -17,9 +17,23 @@ export const Preloader = ({ resolve }) => {
   return null;
 };
 
+// #. 사용자 정의 usePreloader Hook
+export const usePreloader = (resolve) => {
+  const preloadContext = useContext(PreloadContext);
+  if (!preloadContext) return null; // #. context 값이 유효하지 않으면 아무것도 하지 않음
+  if (preloadContext.done) return null; // #. 이미 작업이 끝났다면 아무것도 하지 않음
+
+  // #. promises 배열에 프로미스 등록
+  //    (resolve 함수가 프로미스를 반환하지 않더라도 프로미스로 취급하기 위해 Promise.resolve 함수 사용하여 등록)
+  preloadContext.promises.push(Promise.resolve(resolve()));
+};
+
 /**
  * PreloadContext : 서버 사이드 렌더링을 하는 과정에서 처리해야 할 작업들을 실행하고 만약 기다려야 하는 프로미스가 있으면 수집한다.
  *                  모든 프로미스를 수집한 뒤 수집된 프로미스들이 끝날때까지 기다렸다가 그 다음에 다시 렌더링하면 데이터가 채워진 상태로 컴포넌트들이 나타나게 된다.
  *
  * Preloader 컴포넌트 : resolve 함수를 props로 받아 오며 컴포넌트가 렌더링될 때 서버 환경에서만 resolve 함수를 호출해 준다.
+ *
+ * 사용자 정의 usePreloader Hook :
+ *   Preloader 컴포넌트를 사용하여 서버 사이드 렌더링을 하기 전 데이터가 필요한 상황에서 API 를 요청하는 작업을 사용자 정의 usePreloader Hook 을 사용하여 대체 가능.
  */
