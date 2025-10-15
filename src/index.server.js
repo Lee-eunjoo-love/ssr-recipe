4b066e64a70b7f2b576e31654a26195aa335f89c
Lee-eunjoo-love <hychunjae2025@gmail.com>
2025-10-16 06:23:48 +0900

    [청크 파일 추출]

--- src/index.server.js
+++ src/index.server.js
@@ -10,6 +10,10 @@ import { thunk } from "redux-thunk";
 import rootReducer, { rootSaga } from "./modules";
 import PreloadContext from "./lib/PreloadContext";
 import createSagaMiddleware, { END } from "redux-saga";
+import { ChunkExtractor, ChunkExtractorManager } from "@loadable/server"; // #. [청크 파일 경로 추출]
+
+// #. [청크 파일 경로 추출]
+const statsFile = path.resolve("./build/loadable-stats.json");
 
 // 정적 파일 html 내부에 주입
 const manifest = JSON.parse(
@@ -29,7 +33,7 @@ const chunks = Object.keys(manifest.files)
   .map((key) => `<link href="${manifest.files[key]}" rel="stylesheet" />`)
   .join("");*/
 
-function createPage(root, stateScript) {
+/*function createPage(root, stateScript) {
   return `<!DOCTYPE html>
   <html lang="en">
     <head>
@@ -46,6 +50,26 @@ function createPage(root, stateScript) {
         ${stateScript}
         ${chunks}
         <script src="${manifest.files["main.js"]}"></script>
+    </body>
+  </html>`;
+}*/
+// #. [청크 파일 경로 추출]
+function createPage(root, tags) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#000000" />
+        <title>React App</title>
+        ${tags.styles}
+        ${tags.links}
+    </head>
+    <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        <div id="root">${root}</div>
+        ${tags.scripts}
     </body>
   </html>`;
 }
@@ -67,14 +91,21 @@ const serverRender = async (req, res, next) => {
     done: false,
     promises: [],
   };
+
+  // #. [청크 파일 경로 추출]
+  const extractor = new ChunkExtractor({ statsFile });
+
+  // #. [청크 파일 경로 추출]
   const jsx = (
-    <PreloadContext.Provider value={preloadContext}>
-      <Provider store={store}>
-        <StaticRouter location={req.url} context={context}>
-          <App />
-        </StaticRouter>
-      </Provider>
-    </PreloadContext.Provider>
+    <ChunkExtractorManager extractor={extractor}>
+      <PreloadContext.Provider value={preloadContext}>
+        <Provider store={store}>
+          <StaticRouter location={req.url} context={context}>
+            <App />
+          </StaticRouter>
+        </Provider>
+      </PreloadContext.Provider>
+    </ChunkExtractorManager>
   );
   ReactDOMServer.renderToStaticMarkup(jsx); // #. renderToStaticMarkup 으로 한번 렌더링. (Preloader로 넣어 준 함수를 호출하기 위한 목적. 처리 속도가 renderToString 보다 빨라 대신 사용)
   store.dispatch(END); // #. [redux-saga 서버 사이드 렌더링 설정] redux-saga 의 END 액션을 발생시키면 액션을 모니터링하는 사가들이 모두 종료됨
@@ -88,7 +119,17 @@ const serverRender = async (req, res, next) => {
   const root = ReactDOMServer.renderToString(jsx); // 렌더링된 결과물을 문자열로 변환.
   const stateString = JSON.stringify(store.getState()).replace(/</g, "\\u003c");
   const stateScript = `<script>__PRELOADED_STATE__ = ${stateString}</script>`; // #. 리덕스 초기 상태 스크립트로 주입
-  res.send(createPage(root, stateScript)); // 클라이언트에게 결과물을 응답
+
+  // #. [청크 파일 경로 추출]
+  const tags = {
+    scripts: stateScript + extractor.getScriptTags(), // #. 스크립트 앞부분에 리덕스 상태 주입
+    links: extractor.getLinkTags(),
+    styles: extractor.getStyleTags(),
+  };
+
+  console.log("====> tag: ", JSON.stringify(tags));
+
+  res.send(createPage(root, tags)); // 클라이언트에게 결과물을 응답
 };
 
 // 정적 파일 제공 (index: false 옵션을 주어 '/' 경로에서 index.html 을 제공하지 않도록 설정)
